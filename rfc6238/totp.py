26e769eb4875cb4ac869f72574c0b3c3072c6096
Marco Caimi <caimi.marco@protonmail.ch>
2017-03-06 11:40:02 +0100

    [+] Commented TOTP code a bit

--- rfc6238/totp.py
+++ rfc6238/totp.py
@@ -19,11 +19,28 @@ except ImportError as e:
 TS = 30
 
 # strip whitespaces from key if any is present
+"""
+    normalize(key):
+        strip away any whitespaces from a string or bytestring input
+
+    key: the string to be processed. can be a 'str' or 'bytes' string
+"""
 def normalize(key):
     return key.replace(' ', '') if isinstance(key, str) else key.replace(b' ', b'')
 
 # generate TOTP Token as per RFC 6238
-def TOTP(key, digest=hashlib.sha1, timestep=TS, encode_base32=True, casefold=True):
+"""
+    TOTP(key, digest, timestep, timebase, encode_base32, casefold):
+        computes a TOTP token based on the secret key and the current timestamp.
+
+    key: shared secret. Input is a string, it is converted to UTF-8 bytestring automatically
+    digest: hash to be used. by default SHA-1 is employed
+    timestep: TOTP token period. default is 30 sec (Google Auth compatibility value)
+    timebase: point in time from which we compute the timestamp in microseconds. default is 0
+    encode_base32: if True the key is base32 encoded (Google auth), otherwise the key is left as is. default is True
+    casefold: if True, base32 conversion is case-insensitive. defaults to True
+"""
+def TOTP(key, digest=hashlib.sha1, timestep=TS, timebase=0, encode_base32=True, casefold=True):
     # normalize and convert key in proper format
     key = hmac.str2unicode(key)
     key = normalize(key)
@@ -33,7 +50,7 @@ def TOTP(key, digest=hashlib.sha1, timestep=TS, encode_base32=True, casefold=Tru
         key = base64.b32decode(key, casefold=casefold)
 
     # compute timestamp and convert value in unsigned 64 bit integer
-    T0 = 0 # unix epoch
+    T0 = 0 # unix epoch in RFC, 0 in google implementation
     now = floor((datetime.now().timestamp() - T0)/timestep)
 
     # encode TC as unsigned 64bit integer
