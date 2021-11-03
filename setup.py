6b9991b13c995a315f673a72d06de87da6996468
Marco Caimi <caimi.marco@protonmail.ch>
2021-11-03 15:06:12 +0100

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 # install OTP modules
 setup(
         name="python-otp-lib",
-        version="0.1",
+        version="1.0",
         packages=find_packages(),
         author="Marco Caimi",
         author_email="mcaimi@redhat.com",
