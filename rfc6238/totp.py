ee16a277a4784c9e96cb77f82888bc1028ff8022
Marco Caimi <caimi.marco@protonmail.ch>
2021-11-03 15:03:56 +0100

    Dropped compatibility with python 2.6

--- rfc6238/totp.py
+++ rfc6238/totp.py
@@ -5,18 +5,17 @@
 #
 import struct
 import hashlib
-import six
 import os
 from math import floor
 from time import time
 import base64
+from typing import AnyStr, Callable
 try:
     from rfc4226 import hotp
     from rfc2104 import hmac
 except ImportError as e:
     raise e
 
-import unicodedata
 try:
     from urllib.parse import quote, urlencode
 except ImportError:
@@ -24,65 +23,81 @@ except ImportError:
 
 # time step
 # seconds, default value
-TS = 30
+TS: int = 30
 # initial time. default is unix epoch
-EPOCH = 0 # gmtime(0) or Midnight 1 Jan 1970
+EPOCH: int = 0  # gmtime(0) or Midnight 1 Jan 1970
 
-# strip whitespaces from key if any is present
 """
     normalize(key):
         strip away any whitespaces from a string or bytestring input
 
-    key: the string to be processed. can be a 'str' or 'bytes' string
+    key:
+        the string to be processed.
 """
-def normalize(key):
+
+
+def normalize(key: AnyStr) -> AnyStr:
     return key.replace(' ', '') if isinstance(key, str) else key.replace(b' ', b'')
 
-# decode a base32 secret
+
 """
     base32_decode(key):
-        decode a base32 encoded secret. 
+        decode a base32 encoded secret.
         Ensure that the key is properly padded before calling b32decode
 
-    key: secret string to be processed.
+    key:
+        secret string to be processed.
 """
-def base32_decode(key, casefold=True):
+
+
+def base32_decode(key: AnyStr, casefold: bool = True) -> bytes:
     padlen = len(key) % 8
 
     if isinstance(key, str):
-        key = hmac.str2unicode(key)
+        key = hmac.str2bytes(key)
 
     if (padlen > 0):
         key += b'=' * (8 - padlen)
 
     return base64.b32decode(key, casefold=casefold)
 
-# generate TOTP Token as per RFC 6238
+
 """
     TOTP(key, digest, timestep, timebase, encode_base32, casefold):
         computes a TOTP token based on the secret key and the current timestamp.
 
-    key: shared secret. Input is a string, it is converted to UTF-8 bytestring automatically
-    digest: hash to be used. by default SHA-1 is employed
-    timecounter: point in time from epoch for which you want to compute the TOTP. (default is time.time())
-    timestep: TOTP token period. default is 30 sec (Google Auth compatibility value)
-    timebase: point in time from which we compute the timestamp in microseconds. default is EPOCH
-    encode_base32: if True the key is base32 encoded (Google auth), otherwise the key is left as is. default is True
-    casefold: if True, base32 conversion is case-insensitive. defaults to True
+    key:
+        shared secret. Input is a string, it is converted to UTF-8 bytestring automatically
+    digest:
+        hash to be used. by default SHA-1 is employed
+    timecounter:
+        point in time from epoch for which you want to compute the TOTP. (default is time.time())
+    timestep:
+        TOTP token period. default is 30 sec (Google Auth compatibility value)
+    timebase:
+        point in time from which we compute the timestamp in microseconds. default is EPOCH
+    encode_base32:
+        if True the key is base32 encoded (Google auth), otherwise the key is left as is. default is True
+    casefold:
+        if True, base32 conversion is case-insensitive. defaults to True
 """
-def TOTP(key, timecounter=0, digest=hashlib.sha1, timestep=TS, timebase=EPOCH, encode_base32=True, casefold=True, token_len=6):
+
+
+def TOTP(key: AnyStr, timecounter: int = 0, digest: Callable = hashlib.sha1, timestep: int = TS, timebase: int = EPOCH, encode_base32: bool = True, casefold: bool = True, token_len:int = 6) -> str:
     # normalize and convert key in proper format
     key = normalize(key)
     # convert to unicode bytestring
-    key = hmac.str2unicode(key)
+    key = hmac.str2bytes(key)
 
     # google wants the key to be base32 encoded...
     if (encode_base32):
         key = base32_decode(key, casefold=casefold)
 
     # compute timestamp and convert value in unsigned 64 bit integer
-    T0 = timebase # unix epoch in RFC
-    if timecounter==0:
+    T0 = timebase  # unix epoch in RFC
+    if timecounter == -1:
+        now = 12345678  # for testing
+    elif timecounter == 0:
         now = floor(float(time() - T0)/timestep)
     else:
         now = floor(timecounter/timestep)
@@ -108,16 +123,15 @@ def TOTP(key, timecounter=0, digest=hashlib.sha1, timestep=TS, timebase=EPOCH, e
 
     generates a new random base32-encoded key that can be used to generate TOTP codes.
 
-    byte_key: how many random bytes to read from /dev/urandom
-    digest: hash function to apply to the random bytearray before conversion
+    byte_key:
+        how many random bytes to read from /dev/urandom
+    digest:
+        hash function to apply to the random bytearray before conversion
 """
-def get_random_base32_key(byte_key=32, digest=hashlib.sha1):
-    if six.PY3:
-        random_b32 = base64.b32encode(digest(os.urandom(byte_key)).digest())
-    else:
-        random_b32 = six.b(str(base64.b32encode(digest(os.urandom(byte_key)).digest())))
 
-    return random_b32
+
+def get_random_base32_key(byte_key: int = 32, digest: Callable = hashlib.sha1) -> bytes:
+    return base64.b32encode(digest(os.urandom(byte_key)).digest())
 
 
 """
@@ -127,15 +141,21 @@ def get_random_base32_key(byte_key=32, digest=hashlib.sha1):
         https://github.com/google/google-authenticator/wiki/Key-Uri-Format
         adapted from pyotp library code
 
-    secret: the totp secret used to generate the URI
-    name: name of the account
-    issuer_name: the name of the OTP issuer; this will be the organization title of the OTP entry in Authenticator
-    algorithm: the algorithm used in the OTP generation.
-    digits: the length of the OTP generated code.
-    period: the number of seconds the OTP generator is set to expire every code.
+    secret:
+        the totp secret used to generate the URI
+    name:
+        name of the account
+    issuer_name:
+        the name of the OTP issuer; this will be the organization title of the OTP entry in Authenticator
+    algorithm:
+        the algorithm used in the OTP generation.
+    digits:
+        the length of the OTP generated code.
+    period:
+        the number of seconds the OTP generator is set to expire every code.
 """
 
-def build_uri(secret, name, issuer_name=None, digest=None, digits=None, period=None):
+def build_uri(secret: AnyStr, name: str, issuer_name: str = None, digest: str = None, digits: int = None, period: int = None) -> str:
     # Handling values different from defaults
     chosen_digest = digest if (digest is not None and digest != 'sha1') else 'sha1'
     token_length = digits if (digits is not None and digits != 6) else 6
@@ -160,4 +180,3 @@ def build_uri(secret, name, issuer_name=None, digest=None, digits=None, period=N
 
     uri = base_uri.format('totp', label, urlencode(url_args).replace("+", "%20"))
     return uri
-
