03cb59029a6c3bd69fcb43229774adb699558b2c
Marco Caimi <caimi.marco@protonmail.ch>
2022-03-01 13:44:38 +0100

    Updated HMAC module: pylint fixes

--- rfc2104/hmac.py
+++ rfc2104/hmac.py
@@ -3,7 +3,7 @@
 #   HMAC generation library
 #   Marco Caimi <mcaimi@redhat.com>
 #
-# calculate HMAC as per RFC2104
+''' calculate HMAC as per RFC2104 '''
 
 import hashlib as hashes
 from typing import AnyStr, Callable
@@ -13,21 +13,21 @@ BLOCK_LEN: int = 64  # bytes
 INNER_PAD: bytes = 0x36
 OUTER_PAD: bytes = 0x5C
 
-"""
-    HMAC(key, message, digest_function, unicode_convert)
-        Generates the HMAC code from the supplied (key,message) pair
 
-    key:
-        HMAC hashing key. Needs to be a byte-encoded string (UTF-8)
-    message:
-        Plaintext of the message you want to get the HMAC out of
-    digest_function:
-        Hashing algorithm to use to compute the HMAC. Default is SHA-1
-"""
+def hmac(key: AnyStr, message: AnyStr, digest_function: Callable = hashes.sha1) -> bytes:
+    """
+        hmac(key, message, digest_function, unicode_convert)
+            Generates the hmac code from the supplied (key,message) pair
 
+        key:
+            hmac hashing key. Needs to be a byte-encoded string (UTF-8)
+        message:
+            Plaintext of the message you want to get the hmac out of
+        digest_function:
+            Hashing algorithm to use to compute the hmac. Default is SHA-1
+    """
 
-def HMAC(key: AnyStr, message: AnyStr, digest_function: Callable = hashes.sha1) -> bytes:
-    # HMAC IS:
+    # hmac IS:
     #   H(key XOR OUTER_PAD, H(key XOR INNER_PAD, message))
 
     # check input type (must be UTF-8 encoded byte strings)
@@ -35,10 +35,10 @@ def HMAC(key: AnyStr, message: AnyStr, digest_function: Callable = hashes.sha1) 
     message = str2bytes(message)
 
     # limit key length to SHA-1 hash size
-    kl = len(key)
-    pad = BLOCK_LEN - kl
-    hmac_key = key if (kl <= BLOCK_LEN) else digest_function(key).digest()
-    key_plaintext_array = ([x for x in hmac_key] + [0x00 for i in range(0, pad)])
+    key_len = len(key)
+    pad = BLOCK_LEN - key_len
+    hmac_key = key if (key_len <= BLOCK_LEN) else digest_function(key).digest()
+    key_plaintext_array = (list(hmac_key) + [0x00 for i in range(0, pad)])
 
     # compute inner element and hash
     inner_cyphertext = [x ^ INNER_PAD for x in key_plaintext_array]
@@ -49,25 +49,24 @@ def HMAC(key: AnyStr, message: AnyStr, digest_function: Callable = hashes.sha1) 
     # compute outer element and hash
     outer_cyphertext = [x ^ OUTER_PAD for x in key_plaintext_array]
     outer_element = bytes(outer_cyphertext) + inner_hmac
-    hmac = digest_function(outer_element).digest()
+    hmac_digest = digest_function(outer_element).digest()
 
-    # return computed HMAC
-    return hmac
-
-
-"""
-    str2bytes(message):
-        converts a python string into an UTF-8 encoded bytestring.
-
-    message:
-        string to be checked and converted
-
-    output is always a string of BYTES.
-
-"""
+    # return computed hmac
+    return hmac_digest
 
 
 def str2bytes(message: AnyStr) -> bytes:
+    """
+        str2bytes(message):
+            converts a python string into an UTF-8 encoded bytestring.
+
+        message:
+            string to be checked and converted
+
+        output is always a string of BYTES.
+
+    """
+
     # convert string to unicode if needed
     if isinstance(message, str):
         message = message.encode('UTF-8')
@@ -75,41 +74,41 @@ def str2bytes(message: AnyStr) -> bytes:
     return message
 
 
-"""
-    hmac_to_string(hmac, delimiter):
-        returns a string representation of the HMAC bytestring supplied
+def hmac_to_string(hmac_digest: bytes, delimiter: str = "") -> str:
+    """
+        hmac_to_string(hmac, delimiter):
+            returns a string representation of the HMAC bytestring supplied
 
-    hmac:
-        computed HMAC bytestring
-    delimitier:
-        delimiter character between hash digits. defaults is an empty char
-"""
+        hmac:
+            computed HMAC bytestring
+        delimitier:
+            delimiter character between hash digits. defaults is an empty char
+    """
 
+    if isinstance(hmac_digest, bytes):
+        return delimiter.join([f"{x:02x}" for x in hmac_digest])
 
-def hmac_to_string(hmac: bytes, delimiter: str = "") -> str:
-    if (isinstance(hmac, bytes)):
-        return delimiter.join(["%02x" % (x) for x in hmac])
-    else:
-        raise TypeError("hmac.hmac_to_string(): Incorrect input type. Expected [bytes], got [%s]" % hmac.__class__)
-
-
-"""
-    string_to_hmac(str_hmac, delimiter):
-        convert an hex string into a bytestring HMAC value
-
-    str_hmac:
-        HMAC in string format (hexadecimal)
-    delimiter:
-        hex values delimiter. default is an empty char
-"""
+    raise TypeError(f"hmac.hmac_to_string(): Incorrect input type.\
+            Expected [bytes], got {hmac.__class__}")
 
 
 def string_to_hmac(str_hmac: str, delimiter: str = "") -> bytes:
-    if (isinstance(str_hmac, str)):
+    """
+        string_to_hmac(str_hmac, delimiter):
+            convert an hex string into a bytestring HMAC value
+
+        str_hmac:
+            HMAC in string format (hexadecimal)
+        delimiter:
+            hex values delimiter. default is an empty char
+    """
+
+    if isinstance(str_hmac, str):
         # remove delimiters if any
         if delimiter != "":
             str_hmac = ''.join(str_hmac.split(delimiter))
         # rebuild bytestring
-        return bytes([x for x in [int(str_hmac[y:y+2], 16) for y in range(0, len(str_hmac), 2)]])
-    else:
-        raise TypeError("hmac.string_to_hmac(): Incorrect input type. Expected [str], got [%s]" % str_hmac.__class__)
+        return bytes(list(int(str_hmac[y: y + 2], 16) for y in range(0, len(str_hmac), 2)))
+
+    raise TypeError(f"hmac.string_to_hmac(): Incorrect input type.\
+            Expected [str], got {str_hmac.__class__}")
--- rfc2104/test_hmac.py
+++ rfc2104/test_hmac.py
@@ -1,77 +1,77 @@
 #!/usr/bin/env python
-# HMAC Test Vector
-# Refer here for test vectors: https://datatracker.ietf.org/doc/html/rfc2104
+''' HMAC Test Vector
+Refer here for test vectors: https://datatracker.ietf.org/doc/html/rfc2104 '''
 
 import hashlib as hl
-import rfc2104.hmac as hm
 import pytest
+import rfc2104.hmac as hm
 
 # test vectors and expected results
 test_vectors = {'case1':
-                    {'key': b'\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b',
-                     'data': 'Hi There',
-                     'result_md5': '9294727a3638bb1c13f48ef8158bfc9d',
-                     'result_sha1': '675b0b3a1b4ddf4e124872da6c2f632bfed957e9'},
+                {'key': b'\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b\x0b',
+                 'data': 'Hi There',
+                 'result_md5': '9294727a3638bb1c13f48ef8158bfc9d',
+                 'result_sha1': '675b0b3a1b4ddf4e124872da6c2f632bfed957e9'},
                 'case2':
                     {'key': 'Jefe',
                      'data': 'what do ya want for nothing?',
                      'result_md5': '750c783e6ab0b503eaa86e310a5db738',
-                     'result_sha1': 'effcdf6ae5eb2fa2d27416d5f184df9c259a7c79'}
-                }
+                     'result_sha1': 'effcdf6ae5eb2fa2d27416d5f184df9c259a7c79'}}
 
 # byte-string conversion test vectors
 conv_test_vectors = {'sha1':
-                        {'byte_rep': b'\xef\xfc\xdfj\xe5\xeb/\xa2\xd2t\x16\xd5\xf1\x84\xdf\x9c%\x9a|y',
-                         'string_rep': 'effcdf6ae5eb2fa2d27416d5f184df9c259a7c79'},
-                    'md5':
-                        {'byte_rep': b'u\x0cx>j\xb0\xb5\x03\xea\xa8n1\n]\xb78',
-                         'string_rep': '750c783e6ab0b503eaa86e310a5db738'}
-                    }
+                     {'byte_rep': b'\xef\xfc\xdfj\xe5\xeb/\xa2\xd2t\x16\xd5\xf1\x84\xdf\x9c%\x9a|y',
+                      'string_rep': 'effcdf6ae5eb2fa2d27416d5f184df9c259a7c79'},
+                     'md5':
+                     {'byte_rep': b'u\x0cx>j\xb0\xb5\x03\xea\xa8n1\n]\xb78',
+                         'string_rep': '750c783e6ab0b503eaa86e310a5db738'}}
 
-# test HMAC routines with MD5 as digest function
+
 def test_hmac_md5() -> bool:
-    for k, v in test_vectors.items():
-        k = v.get('key')
-        m = v.get('data')
-        r = v.get('result_md5')
+    """ test HMAC routines with MD5 as digest function """
+    for key, value in test_vectors.items():
+        key = value.get('key')
+        message = value.get('data')
+        result = value.get('result_md5')
 
-        assert hm.hmac_to_string(hm.HMAC(k, m, digest_function=hl.md5)) == r
+        assert hm.hmac_to_string(hm.hmac(key, message, digest_function=hl.md5)) == result
 
 
-# test HMAC routines with SHA1 as digest function
 def test_hmac_sha1() -> bool:
-    for k, v in test_vectors.items():
-        k = v.get('key')
-        m = v.get('data')
-        r = v.get('result_sha1')
+    """ test HMAC routines with SHA1 as digest function  """
+    for key, value in test_vectors.items():
+        key = value.get('key')
+        message = value.get('data')
+        result = value.get('result_sha1')
 
-        assert hm.hmac_to_string(hm.HMAC(k, m)) == r
+        assert hm.hmac_to_string(hm.hmac(key, message)) == result
 
 
-# test HMAC bytes to string conversion
 def test_hmac_to_string() -> bool:
-    for k, v in conv_test_vectors.items():
-        br = v.get('byte_rep')
-        sr = v.get('string_rep')
+    """ test HMAC bytes to string conversion """
+    for _, value in conv_test_vectors.items():
+        byte_result = value.get('byte_rep')
+        string_result = value.get('string_rep')
 
-        assert hm.hmac_to_string(br) == sr
+        assert hm.hmac_to_string(byte_result) == string_result
 
 
-# test HMAC string to bytes conversion
 def test_string_to_hmac() -> bool:
-    for k, v in conv_test_vectors.items():
-        br = v.get('byte_rep')
-        sr = v.get('string_rep')
+    """ test HMAC string to bytes conversion """
+    for _, value in conv_test_vectors.items():
+        byte_result = value.get('byte_rep')
+        string_result = value.get('string_rep')
 
-        assert hm.string_to_hmac(sr) == br
+        assert hm.string_to_hmac(string_result) == byte_result
 
 
-# test exceptions
 def test_hmac_to_str_wrong_input() -> bool:
+    """ test hmac function with a wrongly set byte array input """
     with pytest.raises(TypeError):
         hm.hmac_to_string("123")
 
 
 def test_str_to_hmac_wrong_input() -> bool:
+    """ test hmac function with a wrongly set string input """
     with pytest.raises(TypeError):
         hm.string_to_hmac(123)
