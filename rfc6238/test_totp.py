fedb6241bb391746b0bae20bff725df8ce893b70
Marco Caimi <caimi.marco@protonmail.ch>
2022-03-01 14:16:21 +0100

    Updated TOTP module: some Pylint fixes

--- rfc6238/test_totp.py
+++ rfc6238/test_totp.py
@@ -1,47 +1,49 @@
 #!/usr/bin/env python
-# TOTP Test Scenarios
+""" TOTP Test Scenarios """
 
-import rfc6238.totp as totp
 import base64
+from rfc6238 import totp
 
 # test vectors
-key = "ORSXG5A="
-result_rfc = "285265"
-result_google = "042328"
+KEY = "ORSXG5A="
+RESULT_RFC = "285265"
+RESULT_GOOGLE = "042328"
 
 
-# test normalize
 def test_normalize_string() -> bool:
+    """ string normalization test case """
     input_str = "String With Spaces"
     output_str = "StringWithSpaces"
     assert totp.normalize(input_str) == output_str
 
 
 def test_normalize_bytes() -> bool:
+    """ byte normalization test case """
     input_bytes = b'\x01 \x02 \x03'
     output_bytes = b'\x01\x02\x03'
     assert totp.normalize(input_bytes) == output_bytes
 
 
-# test base32 decode
 def test_base32_decode() -> bool:
-    q = base64.b32decode(key, True)
-    assert totp.base32_decode(key, True) == q
+    """ base32 decode test case """
+    result = base64.b32decode(KEY, True)
+    assert totp.base32_decode(KEY, True) == result
 
 
-# test TOTP code generation
 def test_google_totp_code_generation() -> bool:
-    z = totp.TOTP(key, timecounter=-1, encode_base32=True)
-    assert str(z) == result_google
+    """ test TOTP token generation (google variant) """
+    result = totp.totp(KEY, timecounter=-1, encode_base32=True)
+    assert str(result) == RESULT_GOOGLE
 
 
 def test_rfc_totp_code_generation() -> bool:
-    z = totp.TOTP(key, timecounter=-1, encode_base32=False)
-    assert str(z) == result_rfc
+    """ test TOTP token generation (rfc variant) """
+    result = totp.totp(KEY, timecounter=-1, encode_base32=False)
+    assert str(result) == RESULT_RFC
 
 
-# test uri builder method
 def test_uri_builder() -> bool:
-    z = totp.TOTP(key, timecounter=-1)
+    """ uri_builder test case """
+    result = totp.totp(KEY, timecounter=-1)
     final_uri = "otpauth://totp/pytest?secret=042328&algorithm=SHA1&digits=6&period=30"
-    assert totp.build_uri(z, "pytest") == final_uri
+    assert totp.build_uri(result, "pytest") == final_uri
--- rfc6238/totp.py
+++ rfc6238/totp.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 #
-#   TOTP Token Generation library (RFC6238)
-#   v0.1 - Marco Caimi <mcaimi@redhat.com>
-#
+""" TOTP Token Generation library (RFC6238)
+v0.1 - Marco Caimi <mcaimi@redhat.com> """
+
 import struct
 import hashlib
 import os
@@ -27,135 +27,129 @@ TS: int = 30
 # initial time. default is unix epoch
 EPOCH: int = 0  # gmtime(0) or Midnight 1 Jan 1970
 
-"""
-    normalize(key):
-        strip away any whitespaces from a string or bytestring input
-
-    key:
-        the string to be processed.
-"""
-
 
 def normalize(key: AnyStr) -> AnyStr:
+    """
+        normalize(key):
+            strip away any whitespaces from a string or bytestring input
+
+        key:
+            the string to be processed.
+    """
     return key.replace(' ', '') if isinstance(key, str) else key.replace(b' ', b'')
 
 
-"""
-    base32_decode(key):
-        decode a base32 encoded secret.
-        Ensure that the key is properly padded before calling b32decode
-
-    key:
-        secret string to be processed.
-"""
-
-
 def base32_decode(key: AnyStr, casefold: bool = True) -> bytes:
+    """
+        base32_decode(key):
+            decode a base32 encoded secret.
+            Ensure that the key is properly padded before calling b32decode
+
+        key:
+            secret string to be processed.
+    """
     padlen = len(key) % 8
 
     if isinstance(key, str):
         key = hmac.str2bytes(key)
 
-    if (padlen > 0):
+    if padlen > 0:
         key += b'=' * (8 - padlen)
 
     return base64.b32decode(key, casefold=casefold)
 
 
-"""
-    TOTP(key, digest, timestep, timebase, encode_base32, casefold):
-        computes a TOTP token based on the secret key and the current timestamp.
+def totp(key: AnyStr, timecounter: int = 0, digest: Callable = hashlib.sha1, timestep: int = TS, timebase: int = EPOCH, encode_base32: bool = True, casefold: bool = True, token_len:int = 6) -> str:
+    """
+        TOTP(key, digest, timestep, timebase, encode_base32, casefold):
+            computes a TOTP token based on the secret key and the current timestamp.
 
-    key:
-        shared secret. Input is a string, it is converted to UTF-8 bytestring automatically
-    digest:
-        hash to be used. by default SHA-1 is employed
-    timecounter:
-        point in time from epoch for which you want to compute the TOTP. (default is time.time())
-    timestep:
-        TOTP token period. default is 30 sec (Google Auth compatibility value)
-    timebase:
-        point in time from which we compute the timestamp in microseconds. default is EPOCH
-    encode_base32:
-        if True the key is base32 encoded (Google auth), otherwise the key is left as is. default is True
-    casefold:
-        if True, base32 conversion is case-insensitive. defaults to True
-"""
+        key:
+            shared secret. Input is a string, it is converted to UTF-8 bytestring automatically
+        digest:
+            hash to be used. by default SHA-1 is employed
+        timecounter:
+            point in time from epoch for which you want to compute the TOTP. (default is time.time())
+        timestep:
+            TOTP token period. default is 30 sec (Google Auth compatibility value)
+        timebase:
+            point in time from which we compute the timestamp in microseconds. default is EPOCH
+        encode_base32:
+            if True the key is base32 encoded (Google auth), otherwise the key is left as is. default is True
+        casefold:
+            if True, base32 conversion is case-insensitive. defaults to True
+    """
 
-
-def TOTP(key: AnyStr, timecounter: int = 0, digest: Callable = hashlib.sha1, timestep: int = TS, timebase: int = EPOCH, encode_base32: bool = True, casefold: bool = True, token_len:int = 6) -> str:
     # normalize and convert key in proper format
     key = normalize(key)
     # convert to unicode bytestring
     key = hmac.str2bytes(key)
 
     # google wants the key to be base32 encoded...
-    if (encode_base32):
+    if encode_base32:
         key = base32_decode(key, casefold=casefold)
 
     # compute timestamp and convert value in unsigned 64 bit integer
-    T0 = timebase  # unix epoch in RFC
+    time_zero = timebase  # unix epoch in RFC
     if timecounter == -1:
         now = 12345678  # for testing
     elif timecounter == 0:
-        now = floor(float(time() - T0)/timestep)
+        now = floor(float(time() - time_zero) / timestep)
     else:
-        now = floor(timecounter/timestep)
+        now = floor(timecounter / timestep)
 
     # encode TC as unsigned 64bit integer
-    tc = struct.pack(">Q", now)
+    time_counter = struct.pack(">Q", now)
 
     # compute HOTP(key, TC)
-    totp_value = hotp.HOTP(key, tc, digest=digest, token_len=token_len)
+    totp_value = hotp.hotp(key, time_counter, digest=digest, token_len=token_len)
 
     # HOTP result is an integer resulting from a modulo operation: check for result length
     # if less than 'token_len', left-pad with zeroes
-    l = len(str(totp_value))
-    if (l < token_len):
-        totp_value = '0' * (token_len - l) + str(totp_value)
+    length = len(str(totp_value))
+    if length < token_len:
+        totp_value = '0' * (token_len - length) + str(totp_value)
 
     # return totp value
     return totp_value
 
 
-"""
-    get_random_base32_key(byte_len, digest)
-
-    generates a new random base32-encoded key that can be used to generate TOTP codes.
-
-    byte_key:
-        how many random bytes to read from /dev/urandom
-    digest:
-        hash function to apply to the random bytearray before conversion
-"""
-
-
 def get_random_base32_key(byte_key: int = 32, digest: Callable = hashlib.sha1) -> bytes:
+    """
+        get_random_base32_key(byte_len, digest)
+
+        generates a new random base32-encoded key that can be used to generate TOTP codes.
+
+        byte_key:
+            how many random bytes to read from /dev/urandom
+        digest:
+            hash function to apply to the random bytearray before conversion
+    """
     return base64.b32encode(digest(os.urandom(byte_key)).digest())
 
 
-"""
-    Returns the provisioning URI for the OTP.
-
-    See also:
-        https://github.com/google/google-authenticator/wiki/Key-Uri-Format
-        adapted from pyotp library code
-
-    secret:
-        the totp secret used to generate the URI
-    name:
-        name of the account
-    issuer_name:
-        the name of the OTP issuer; this will be the organization title of the OTP entry in Authenticator
-    algorithm:
-        the algorithm used in the OTP generation.
-    digits:
-        the length of the OTP generated code.
-    period:
-        the number of seconds the OTP generator is set to expire every code.
-"""
-
 def build_uri(secret: AnyStr, name: str, issuer_name: str = None, digest: str = None, digits: int = None, period: int = None) -> str:
+    """
+        Returns the provisioning URI for the OTP.
+
+        See also:
+            https://github.com/google/google-authenticator/wiki/Key-Uri-Format
+            adapted from pyotp library code
+
+        secret:
+            the totp secret used to generate the URI
+        name:
+            name of the account
+        issuer_name:
+            the name of the OTP issuer; this will be the organization title of the OTP entry in Authenticator
+        algorithm:
+            the algorithm used in the OTP generation.
+        digits:
+            the length of the OTP generated code.
+        period:
+            the number of seconds the OTP generator is set to expire every code.
+    """
+
     # Handling values different from defaults
     chosen_digest = digest if (digest is not None and digest != 'sha1') else 'sha1'
     token_length = digits if (digits is not None and digits != 6) else 6
